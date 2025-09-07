b2a85d3054c494cd7c3d08745956b9e22eb3936e
adbik <077bei015.bikash@pcampus.edu.np>
2025-09-07 15:43:44 +0545

    authors query parameter requirement fulfilled

--- controllers/mainController.ts
+++ controllers/mainController.ts
@@ -1,16 +1,80 @@
 import { isKeyObject } from "util/types";
 import { db } from "../index.js";
+import { queryObjects } from "v8";
 
 export async function getAllAuthors(queryObject, callback){
-    let sql = "SELECT * FROM authors WHERE 1=1";
+    let sql = "SELECT * FROM authors WHERE 1=1 ?";
     const params = [];
+    const whereClause = [];
+    sql = `
+        WITH booksByAuthor AS (
+            SELECT author_id, COUNT(*) bookCount
+            FROM books
+            GROUP BY author_id) 
+        SELECT a.* , bba.bookCount
+        FROM authors a
+        LEFT JOIN booksByAuthor bba
+        ON bba.author_id = a.id
+    `
 
-    for (const [key, value] of Object.entries(queryObject)){
-        sql+=` AND ${key} = ? `;
-        params.push(value);
+    if (queryObject.name){
+        sql+=` AND a.name LIKE ?`;
+        params.push(`%${queryObject.name}%`);
     }
 
+    if (queryObject.sortAuthorsByBooks === "true"){
+        const order = queryObject.order?.toUpperCase() === 'ASC' ? "ASC" : "DESC";
+        sql += ` ORDER BY bba.bookCount ${order}`;
+    }
+
+    // for (const [key, value] of Object.entries(queryObject)){
+
+    //     if()
+
+    //     switch(key){
+
+    //         case('name'):{
+    //             params.push(` AND name LIKE %${value}%`);
+    //             break;
+    //         }
+
+    //         case('sort'):{
+    //             if(value === 'true')
+    //                 sql = `
+    //                     WITH booksByAuthor AS (
+    //                         SELECT author_id, COUNT(*) bookCount
+    //                         FROM books
+    //                         GROUP BY author_id) 
+    //                     SELECT a.* , bba.bookCount
+    //                     FROM authors a
+    //                     LEFT JOIN booksByAuthor bba
+    //                     WHERE ?
+    //                     ON bba.author_id = a.id
+    //                     ORDER BY bba.bookCount ?
+    //                 `
+    //             }
+    //             break;
+    //         }
+    //     }
+
+    // if (!queryObject.name){
+    //     const params = [];
+    //     params.push(" 1 = 1")
+    // }
+    // if (queryObject.sort ==='true')
+    // {if (!queryObject.order){
+    //     params.push(" DESC")
+    // }
+    // else if(queryObject.order == "ASC"){
+    //     params.push("ASC")
+    // }
+    // else{
+    //     params.push("DESC")
+    // }}
+
+    
     db.all(sql, params , (err, rows) =>{
+        console.log(sql, params);
         if (err) return callback(err);
         callback(null, rows)
     })
